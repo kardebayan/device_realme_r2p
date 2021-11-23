f24e30cd7b4b26ebe28e15ac850f5b685c2dfbde
ad1tyas1ngh <adityasingh1768@gmail.com>
2021-11-23 07:39:04 +0530

    r2p: Switch to GCamGO
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- device.mk
+++ device.mk
@@ -152,7 +152,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     libstdc++.vendor \
-    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
@@ -200,6 +199,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
+
+# GCamGO
+$(call inherit-product, packages/apps/GCamGO/camera.mk)
 
 # GPS
 PRODUCT_PACKAGES += \
